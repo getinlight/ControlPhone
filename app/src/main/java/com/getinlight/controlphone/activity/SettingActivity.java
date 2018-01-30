098cac3e298676dd409bd58811fb106d54db0896
getinlight <1123256466@qq.com>
2018-01-30 17:36:46 +0800

    修改

--- app/src/main/java/com/getinlight/controlphone/activity/SettingActivity.java
+++ app/src/main/java/com/getinlight/controlphone/activity/SettingActivity.java
@@ -2,8 +2,10 @@ package com.getinlight.controlphone.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.getinlight.controlphone.R;
+import com.getinlight.controlphone.view.SettingItemView;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        SettingItemView siv_update = findViewById(R.id.siv_update);
+        siv_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                siv_update.setChecked(!siv_update.isChecked());
+                --10--
+            }
+        });
+
     }
 }
--- app/src/main/java/com/getinlight/controlphone/view/SettingItemView.java
+++ app/src/main/java/com/getinlight/controlphone/view/SettingItemView.java
@@ -15,6 +15,10 @@ import com.getinlight.controlphone.R;
 
 public class SettingItemView extends RelativeLayout {
 
+    private CheckBox cb_box;
+    private TextView tv_des;
+    private TextView tv_title;
+
     public SettingItemView(Context context) {
         this(context, null);
     }
@@ -27,14 +31,27 @@ public class SettingItemView extends RelativeLayout {
         super(context, attrs, defStyleAttr);
         View.inflate(context, R.layout.setting_item_view, this);
 
-        TextView tv_title = findViewById(R.id.tv_title);
-        TextView tv_des = findViewById(R.id.tv_des);
-        CheckBox cb_box = findViewById(R.id.cb_update);
-
-
 //        View view = View.inflate(context, R.layout.setting_item_view, this);
 //        this.addView(view);
 
+        tv_title = findViewById(R.id.tv_title);
+        tv_des = findViewById(R.id.tv_des);
+        cb_box = findViewById(R.id.cb_update);
+
+
+    }
+
+    public boolean isChecked() {
+        return cb_box.isChecked();
+    }
+
+    public void setChecked(boolean checked) {
+        cb_box.setChecked(checked);
+        if (checked) {
+            tv_des.setText("自动更新已开启");
+        } else {
+            tv_des.setText("自动更新已关闭");
+        }
     }
 
 }
